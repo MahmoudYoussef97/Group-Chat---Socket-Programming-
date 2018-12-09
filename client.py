e590319f7173fa2c253eb60e99fc75bc23dc447e
Mahmoud Youssef <MahmoudYoussef0097@gmail.com>
2018-12-09 02:50:49 +0200

    Final

---
+++ client.py
@@ -0,0 +1,34 @@
+""" Client Side of the chat room in P2P """
+import socket       # import Socket for P2P communication
+import threading    #import threading for using Multithread
+
+"""AF_INET is the address family of the socket. This is used when we have an Internet Domain with 
+any two hosts, SOCK_STREAM means that data or characters are read in a continuous flow. TCP """
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()             # Return a string containing the hostname of the machine
+port = 2424
+client_socket.connect((host, port))     # Connect the client to the server socket
+
+""" Receiving function """
+def receive(client):
+    serverDown = False
+    while True and (not serverDown):            # If the server is available then go
+        message = client.recv(2048).decode()    # Receiving ...
+        if not message:                         # If there's any failure while receiving
+            print('Server is Down. You are now Disconnected. Press enter to exit...')
+            exit(0)
+        print(message)
+
+""" Sending function """
+def send(client):
+    while True:
+        message = input()
+        print("[Me]: " + message)       # Showing the message on the prompt
+        client.send(message.encode())   # Send the message to the server as to be send to other clients
+
+
+if __name__ == '__main__':
+    threading.Thread(target=receive, args=(client_socket,)).start()     # Making a thread for sending
+    threading.Thread(target=send, args=(client_socket,)).start()        # Making a thread for receiving
+
---
+++ server.py
@@ -0,0 +1,61 @@
+""" Server Side of the chat room in P2P """
+import socket   # import Socket for P2P communication
+import threading    #import threading for using Multithread
+
+
+"""AF_INET is the address family of the socket. This is used when we have an Internet Domain with 
+any two hosts, SOCK_STREAM means that data or characters are read in a continuous flow."""
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()         # Return a string containing the hostname of the machine
+port = 2424
+server_socket.bind((host, port))    # Bind the server with ip address and the port
+server_socket.listen(10)            # Listen for 10 active connections.
+clients = []                        # Client List
+
+""" Making a thread for each client as all clients work in parallel  """
+def client_thread(conn, address):
+    welcome = "[Server]: Welcome to our group chat\nPlease Enter Your Name:"    # Welcome Message
+    conn.send(welcome.encode())             # Send the message to all clients
+    name = conn.recv(2048).decode()         # Receive the name of each client
+    enter = name + " has joined the room"   # Client Entered the room
+    broadcast(enter, conn)                  # Send to other clients that the user has entered the room
+
+    while True:
+        try:
+            message = conn.recv(2048).decode()              # Receiving a message from a client
+            if message:                                     # If there's a valid message then send it to other clients
+                print(str(address) + ": " + message)
+                broadcast_message = "[" + name + "]" + ": " + message
+                broadcast(broadcast_message, conn)
+            else:                                           # If not, then there's a failure so close the connection
+                print(str(address)+": disconnected")
+                broadcast_message = "[" + name + "]" +" has disconnected "
+                broadcast(broadcast_message, conn)
+                conn.close()
+                delete(conn)
+        except:
+            continue
+
+""" Sending Messages to all users except the sender """
+
+def broadcast(messages, connection):
+    for client in clients:
+        if client != connection:
+            try:
+                client.send(messages.encode())
+            except:     # If any failure happened from the client receiving close the connection
+                connection.close()
+
+""" Deleting the client from the client list and close the connection """
+def delete(conn):
+    if conn in clients:
+        clients.remove(conn)
+
+while True:
+
+    conn, address = server_socket.accept()  # Accept the connection from client
+    clients.append(conn)                    # List of all available clients
+    print(str(address) + " :Connected Successfully")
+    threading.Thread(target=client_thread, args=(conn, address,)).start()   # Making a thread for every client
+
+server_socket.close()       # Closing the server socket
